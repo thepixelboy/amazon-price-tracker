6e7cd84fc93907b49fb59fbd0118ba7099480c3e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-24 18:49:27 +0200

    feat: send email alert when product price bellow target price

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import lxml
+import smtplib
+import os
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+
+TARGET_PRICE = 150
 
 page_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
@@ -14,5 +19,16 @@ soup = BeautifulSoup(website_html, "lxml")
 
 # Get the product price (without currency symbol) as a float
 product_price = float(soup.find(name="span", id="priceblock_ourprice").getText().split("$")[1])
+product_name = soup.find(name="span", id="productTitle").getText().strip()
 
-print(product_price)
+if product_price < TARGET_PRICE:
+    message = f"{product_name} is now {product_price}"
+
+    with smtplib.SMTP(os.getenv("SMTP_SERVER"), port=os.getenv("SMTP_PORT")) as connection:
+        connection.starttls()
+        result = connection.login(os.getenv("EMAIL_ADDRESS"), os.getenv("EMAIL_PASSWORD"))
+        connection.sendmail(
+            from_addr=os.getenv("EMAIL_ADDRESS"),
+            to_addrs=os.getenv("EMAIL_PASSWORD"),
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{product_page}",
+        )
